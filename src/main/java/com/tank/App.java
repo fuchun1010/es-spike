a9bff70d77526210b51e98bbc8adea5cb60876b5
fuchun198435163.com <tank198435@163.com>
2020-07-02 01:05:46 +0800

    [fuchun] #tech support nacos

--- src/main/java/com/tank/App.java
+++ src/main/java/com/tank/App.java
@@ -1,5 +1,6 @@
 package com.tank;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author tank198435163.com
  */
 @SpringBootApplication
+@NacosPropertySource(dataId = "es-spike.yaml")
 public class App {
   public static void main(final String[] args) {
-    SpringApplication.run(App.class);
+    SpringApplication.run(App.class, args);
   }
 }
--- src/main/java/com/tank/config/PrepareConfig.java
+++ src/main/java/com/tank/config/PrepareConfig.java
@@ -1,5 +1,6 @@
 package com.tank.config;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -10,11 +11,14 @@ import org.springframework.context.annotation.Configuration;
  * @author tank198435163.com
  */
 @Configuration
-public class PrepareConfig  {
+public class PrepareConfig {
 
   @Bean(name = "restClient", destroyMethod = "close")
   public RestHighLevelClient initRestHighLevelClient() {
     return new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9201, "http")));
   }
+
+  @NacosValue(value = "${test.name}", autoRefreshed = true)
+  private String applicationName;
 
 }
